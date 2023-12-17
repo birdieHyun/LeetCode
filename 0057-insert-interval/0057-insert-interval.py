7aafc285bc6381ff57be000246148ab65001d63a
임동현 <flaehdan@gmail.com>
2023-12-17 15:06:56 +0900

    Time: 83 ms (55.57%), Space: 19.8 MB (64.17%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,16 +3,18 @@ from typing import List
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-
+        # interval 은 총 세 부분으로 나뉨
+        # 왼쪽, 오른쪽, 병합된 가운데
+        answer = []
         left, right = [], []
 
         for interval in intervals:
             if interval[1] < newInterval[0]:
-                left += interval,
+                left.append(interval)
             elif interval[0] > newInterval[1]:
-                right += interval,
+                right.append(interval)
             else:
-                newInterval[0] = min(newInterval[0], interval[0])
-                newInterval[1] = max(newInterval[1], interval[1])
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
 
         return left + [newInterval] + right
