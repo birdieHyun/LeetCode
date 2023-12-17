2365059dacf79fa1535ef90fa9322c7222c48716
임동현 <flaehdan@gmail.com>
2023-12-17 15:00:05 +0900

    Time: 90 ms (17.95%), Space: 19.8 MB (31.30%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+
+        left, right = [], []
+
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                left += interval,
+            elif interval[0] > newInterval[1]:
+                right += interval,
+            else:
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+
+        return left + [newInterval] + right
